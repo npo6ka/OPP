961101dcf6fda96eeb7b419c658637f6133a8ea0
npo6ka <lenchik.102@mail.ru>
2015-09-26 14:11:31 +0300

    Add .h file

---
+++ OOP_test_application_1/Groups.h
@@ -0,0 +1,87 @@
+#ifndef GROUPSTUDENT_H
+#define GROUPSTUDENT_H
+
+using namespace std;
+
+class Group
+{
+	vector<Student> student;
+public:
+	Group()	{}
+	~Group() {}
+	bool add_student(char *name, char *surname, char *patronymic, int points) {
+		Student stud(name, surname, patronymic, points);
+		if (stud.valid_student()) {
+			student.insert(student.end(), stud);
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	bool rem_student(int num) {
+		if ((int)student.size() >= num && num >=0) {
+			student.erase(student.begin()+num);
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	void destroy() {
+		if (!student.empty()) {
+			student.clear();
+		}
+	}
+	float avg_point() {
+		int sum = 0;
+		for (int i = 0; i < (int)student.size(); i++) {
+			sum += student[i].GetPoint();
+		}
+		return (float)sum / student.size();
+	}
+	void sort_sname() {
+		for (int i = 0; i < (int)student.size() - 1 ; i++) {
+			for (int j = i; j < (int)student.size() - 1; j++) {
+				int k = 0;
+				//i?iaa?ea no?ie
+				do {
+					if (student[j].GetSurname()[k] == student[j+1].GetSurname()[k]) {
+						k++;
+					} else {
+						
+						if (student[j].GetSurname()[k] < student[j+1].GetSurname()[k]) {
+							k = 0;
+						} else {
+							k = -1;
+						}
+					}
+				} while (k > 0 && student[j].GetSurname()[k] != '\0' &&  student[j+1].GetSurname()[k] != '\0');
+				// e = 0 no?iee iaai iiiaiyou ianoaie
+				if (k == 0) {
+					Student stud = student[j];
+					student[j] = student[j+1];
+					student[j+1] = stud;
+				}
+			}
+		}
+	}
+	void sort_point() {
+		for (int i = 0; i < (int)student.size() - 1 ; i++) {
+			for (int j = i; j < (int)student.size() - 1; j++) {
+				if (student[j].GetPoint() < student[j+1].GetPoint()) {
+					Student stud = student[j];
+					student[j] = student[j+1];
+					student[j+1] = stud;
+				}
+			}
+		}
+	}
+	void print_group() {
+		for (int i = 0; i < (int)student.size(); i++) {
+			student[i].print_info();
+		}
+		cout << endl;
+	}
+
+};
+
+#endif
