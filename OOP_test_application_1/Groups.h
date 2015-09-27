32ea090a8b23595ca2838dc571e4a41e216456f5
npo6ka <lenchik.102@mail.ru>
2015-09-28 00:30:43 +0300

    Add fuction in Groups.h

--- OOP_test_application_1/Groups.h
+++ OOP_test_application_1/Groups.h
@@ -7,139 +7,167 @@ using namespace std;
 
 class Group
 {
-	list<Student *> GroupStudent;
-	list<Student *>::iterator FindStudById(int id);
-	list<Student *>::iterator FindStudByRef(Student *stud);
-	list<Student *>::iterator FindStudByName(string name, string sname);
-	bool RmStud(list<Student *>::iterator it);
+    list<Student *> GroupStudent;
+    list<Student *>::iterator FindStudById(int id);
+    list<Student *>::iterator FindStudByRef(Student *stud);
+    list<Student *>::iterator FindStudByName(string name, string sname);
+    bool RmStud(list<Student *>::iterator it);
 public:
-	Group();
-	~Group();
-	Group(const Student &other);
-	bool add_student(Student *stud);
-	bool rem_student (int id);
-	bool rem_student (Student *stud);
-	bool rem_student (string name, string sname);
-	
+    Group();
+    ~Group();
+    Group(const Group &other);
+    bool Add_student(Student *stud);
+    bool Rem_student (int id);
+    bool Rem_student (Student *stud);
+    bool Rem_student (string name, string sname);
+    Student* FindStudent(string name, string sname);
+    Student* FindStudent(int id);
+    bool ValidGroup();
+    list<string> InfoGroup();
+    float AvrGrade();
+    void SortByGrade();
 };
 
-Group::Group()	{}
+Group::Group()    {}
 Group::~Group() {}
-Group::Group(const Student &other) {
-	*this = other;
+Group::Group(const Group &other) {
+    *this = other;
 }
 
-bool Group::add_student(Student *stud) {
-	if (stud->valid_student() && FindStudById(stud->GetId()) == GroupStudent.end()) {
-		GroupStudent.push_front(stud);
-		return 1;
-	} else {
-		return 0;
-	}
+bool Group::Add_student(Student *stud) {
+    Group buff = *this;
+    GroupStudent.push_front(stud);
+    if (!ValidGroup() || FindStudById(stud->GetId()) == GroupStudent.end()) {
+        *this = buff;
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 list<Student *>::iterator Group::FindStudById(int id) {
-	return std::search_n(GroupStudent.begin(), GroupStudent.end(), 1, id, 
-						 [](Student * i, int j) -> bool { 
-							 return (i->GetId() == j); 
-						 });
+    return std::search_n(GroupStudent.begin(), GroupStudent.end(), 1, id, 
+                         [](Student * i, int j) -> bool { 
+                             return (i->GetId() == j); 
+                         });
 }
 list<Student *>::iterator Group::FindStudByRef(Student *stud) {
-	return std::search_n(GroupStudent.begin(), GroupStudent.end(), 1, stud);
+    return std::search_n(GroupStudent.begin(), GroupStudent.end(), 1, stud);
 }
 list<Student *>::iterator Group::FindStudByName(string name, string sname) {
-	string str[2] = {name, sname}; 
-	return std::search_n(GroupStudent.begin(), GroupStudent.end(), 1, str, 
-						 [](const Student * i, const string str[]) -> bool {
-							 return (i->GetName() == str[0] && i->GetSurname() == str[1]);
-						 });
+    string str[2] = {name, sname}; 
+    return std::search_n(GroupStudent.begin(), GroupStudent.end(), 1, str, 
+                         [](const Student * i, const string str[]) -> bool {
+                             return (i->GetName() == str[0] && i->GetSurname() == str[1]);
+                         });
 }
 
 bool Group::RmStud(list<Student *>::iterator it) {
-	if (it != GroupStudent.end() && it._Ptr->_Myval->valid_student()) {
-		GroupStudent.erase(it);
-		return 1;
-	} else {
-		return 0;
-	}
+    if (it != GroupStudent.end()) {
+        GroupStudent.erase(it);
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
-bool Group::rem_student(int id) {
-	return RmStud(FindStudById(id));
+bool Group::Rem_student(int id) {
+    return RmStud(FindStudById(id));
 }
-bool Group::rem_student(Student *stud) {
-	return RmStud(FindStudByRef(stud));
+bool Group::Rem_student(Student *stud) {
+    return RmStud(FindStudByRef(stud));
 }
-bool Group::rem_student(string name, string sname) {
-	return RmStud(FindStudByName(name, sname));
+bool Group::Rem_student(string name, string sname) {
+    return RmStud(FindStudByName(name, sname));
 }
 
-//cout << it._Ptr->_Myval->GetName() << endl;
+Student* Group::FindStudent(string name, string sname) {
+    return *FindStudByName(name, sname);
+}
+Student* Group::FindStudent(int id) {
+    return *FindStudById(id);
+}
 
-/*if ((int)student.size() >= num && num >=0) {
-		student.erase(student.begin()+num);
-		return 1;
-	} else {
-		return 0;
-	}*/
+bool Group::ValidGroup() {
+    for (auto& i: GroupStudent) {
+        if (!i->valid_student()) return 0;
+    }
+    return 1;
+}
 
-/*list<Student> Group::FindStudent(int id)
-{
+list<string> Group::InfoGroup() {
+    list<string> buff;
+    for (auto& i: GroupStudent) {
+        buff.push_back(i->Print_info());
+    }
+    return buff;
+}
 
-}*/
+float Group::AvrGrade() {
+    if (!GroupStudent.size()) {
+        return 0;
+    } else {
+        int Grd = 0;
+        for (auto& i: GroupStudent) {
+            Grd += i->GetGrade();
+        }
+        return (float)Grd/GroupStudent.size();
+    }  
+}
+
+void Group::SortByGrade() {
+    GroupStudent.sort([](const Student *first, const Student *second) -> bool {
+        return (first->GetGrade() < second->GetGrade());
+    });
+}
+
 
 //void destroy() {
-	//	if (!student.empty()) {
-	//		student.clear();
-	//	}
-	//}
-	//float avg_point() {
-	//	int sum = 0;
-	//	for (int i = 0; i < (int)student.size(); i++) {
-	//		sum += student[i].GetPoint();
-	//	}
-	//	return (float)sum / student.size();
-	//}
-	//void sort_sname() {
-	//	for (int i = 0; i < (int)student.size() - 1 ; i++) {
-	//		for (int j = i; j < (int)student.size() - 1; j++) {
-	//			int k = 0;
-	//			do {
-	//				if (student[j].GetSurname()[k] == student[j+1].GetSurname()[k]) {
-	//					k++;
-	//				} else {
-	//					
-	//					if (student[j].GetSurname()[k] < student[j+1].GetSurname()[k]) {
-	//						k = 0;
-	//					} else {
-	//						k = -1;
-	//					}
-	//				}
-	//			} while (k > 0 && student[j].GetSurname()[k] != '\0' &&  student[j+1].GetSurname()[k] != '\0');
-	//			if (k == 0) {
-	//				Student stud = student[j];
-	//				student[j] = student[j+1];
-	//				student[j+1] = stud;
-	//			}
-	//		}
-	//	}
-	//}
-	//void sort_point() {
-	//	for (int i = 0; i < (int)student.size() - 1 ; i++) {
-	//		for (int j = i; j < (int)student.size() - 1; j++) {
-	//			if (student[j].GetPoint() < student[j+1].GetPoint()) {
-	//				Student stud = student[j];
-	//				student[j] = student[j+1];
-	//				student[j+1] = stud;
-	//			}
-	//		}
-	//	}
-	//}
-	//void print_group() {
-	//	for (int i = 0; i < (int)student.size(); i++) {
-	//		student[i].print_info();
-	//	}
-	//	cout << endl;
-	//}
+    //    if (!student.empty()) {
+    //        student.clear();
+    //    }
+    //}
+    //float avg_point() {
+    //    int sum = 0;
+    //    for (int i = 0; i < (int)student.size(); i++) {
+    //        sum += student[i].GetPoint();
+    //    }
+    //    return (float)sum / student.size();
+    //}
+    //void sort_sname() {
+    //    for (int i = 0; i < (int)student.size() - 1 ; i++) {
+    //        for (int j = i; j < (int)student.size() - 1; j++) {
+    //            int k = 0;
+    //            do {
+    //                if (student[j].GetSurname()[k] == student[j+1].GetSurname()[k]) {
+    //                    k++;
+    //                } else {
+    //                    
+    //                    if (student[j].GetSurname()[k] < student[j+1].GetSurname()[k]) {
+    //                        k = 0;
+    //                    } else {
+    //                        k = -1;
+    //                    }
+    //                }
+    //            } while (k > 0 && student[j].GetSurname()[k] != '\0' &&  student[j+1].GetSurname()[k] != '\0');
+    //            if (k == 0) {
+    //                Student stud = student[j];
+    //                student[j] = student[j+1];
+    //                student[j+1] = stud;
+    //            }
+    //        }
+    //    }
+    //}
+    //void sort_point() {
+    //    for (int i = 0; i < (int)student.size() - 1 ; i++) {
+    //        for (int j = i; j < (int)student.size() - 1; j++) {
+    //            if (student[j].GetPoint() < student[j+1].GetPoint()) {
+    //                Student stud = student[j];
+    //                student[j] = student[j+1];
+    //                student[j+1] = stud;
+    //            }
+    //        }
+    //    }
+    //}
 ;
 #endif
--- OOP_test_application_1/OOP_test_application_1.cpp
+++ OOP_test_application_1/OOP_test_application_1.cpp
@@ -4,77 +4,32 @@
 
 using namespace std;
 
-int main()
- {
-	Student ab1("anton", "ivanov", 12);
-	Student ab2("svetlana", "petrova", 7);
-	Student ab3("vlad", "sidorov", 3);
-	std::cout << ab1.Print_info() << endl;
-	std::cout << ab2.Print_info() << endl;
-	std::cout << ab3.Print_info() << endl;
-	Group group;
-	group.add_student(&ab1);
-	group.add_student(&ab2);
-	group.add_student(&ab3);
-	group.rem_student(0);
-	group.rem_student("vlad", "sidorov");
-	group.rem_student(&ab2);
-	//group.rem_student(&ab3);
-
-	/*group.add_student("sidor", "abcda", 10);
-	group.add_student("pidor", "abcdf", 9);
-	group.add_student("sigor", "abcdb", 8);
-	group.add_student("midor", "abcd", 7);
-	cout << group.avg_point() << endl;
-	group.print_group();
-	group.sort_point();
-	group.print_group();
-	group.sort_sname();
-	group.print_group();
-	group.rem_student(1);
-	group.print_group();
-	group.destroy();*/
-	// erasing from list
-	return 0;
+void printGroup(list<string> buff) {
+	for (auto& i: buff) {
+		cout << i << endl;
+	}
+	cout << endl;
 }
 
-
-/*#include <iostream>
-#include <list>
-
-int main ()
-{
-  std::list<int> mylist;
-  std::list<int>::iterator it1,it2;
-
-  // set some values:
-  for (int i=1; i<10; ++i) mylist.push_back(i*10);
-  copy(mylist.begin(), mylist.end(), ostream_iterator<int>(cout,"\n"));
-                              // 10 20 30 40 50 60 70 80 90
-  it1 = it2 = mylist.begin(); // ^^
-  advance (it2,6);            // ^                 ^
-  ++it1;                      //    ^              ^
-  copy(mylist.begin(), mylist.end(), ostream_iterator<int>(cout,"\n"));
-  it1 = mylist.erase (it1);   // 10 30 40 50 60 70 80 90
-                              //    ^           ^
-  copy(mylist.begin(), mylist.end(), ostream_iterator<int>(cout,"\n"));
-  it2 = mylist.erase (it2);   // 10 30 40 50 60 80 90
-                              //    ^           ^
-  copy(mylist.begin(), mylist.end(), ostream_iterator<int>(cout,"\n"));
-  ++it1;                      //       ^        ^
-  --it2;                      //       ^     ^
-  copy(mylist.begin(), mylist.end(), ostream_iterator<int>(cout,"\n"));
-  //mylist.erase (it1, mylist.end());     // 10 30 60 80 90
-                              //        ^
-  for (auto& x: mylist)
-    std::cout << " (" << x << ")";
-
-  copy(mylist.begin(), mylist.end(), ostream_iterator<int>(cout," "));
-  std::cout << "mylist contains:";
-  for (it1=mylist.begin(); it1!=mylist.end(); ++it1)
-    std::cout << ' ' << *it1;
-  std::cout << '\n';
-
-  return 0;
-}*/
+int main()
+ {
+	Student ab1("anton", "ivanov", 7);
+	Student ab2("svetlana", "petrova", 12);
+	Student ab3("vlad", "sidorov", 3);
+	Group group;
+	group.Add_student(&ab1);
+	group.Add_student(&ab2);
+	group.Add_student(&ab3);
+    cout << group.AvrGrade() << endl;
+    printGroup(group.InfoGroup());
+    group.SortByGrade();
+    printGroup(group.InfoGroup());
+	group.FindStudent(2);
+	group.Rem_student(0);
+	group.Rem_student("vlad", "sidorov");
+	group.Rem_student(&ab2);
+    cout << group.AvrGrade() << endl;
+    printGroup(group.InfoGroup());
+	return 0;
+}
 
